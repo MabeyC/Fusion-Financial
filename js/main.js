8fc641cbfff73e47288696dbc82861675cbc93fe
Chris M <chris@chrismabey.dev>
2023-07-18 15:42:37 -0300

    Added favicon, updated form button handling, added small versions of images

--- js/main.js
+++ js/main.js
@@ -10,16 +10,53 @@ window.addEventListener('scroll', function () {
 // Smooth Scrolling
 $('#navbar a, .btn').on('click', function (event) {
   if (this.hash !== '') {
-    event.preventDefault();
-
     const hash = this.hash;
+    const target = $(hash);
 
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top - 100
-      },
-      800
-    );
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').animate(
+        {
+          scrollTop: target.offset().top - 100
+        },
+        800
+      );
+    }
   }
 });
 
+// Form Handling 
+const formEl = document.querySelector('#contactForm');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const phoneInput = document.querySelector('#phone');
+const msgInput = document.querySelector('#message');
+
+const submitBtn = document.querySelector('#submit');
+const resetBtn = document.querySelector('#reset');
+
+resetBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('Form Reset');
+  // Clear Values
+  nameInput.value = '';
+  emailInput.value = '';
+  phoneInput.value = '';
+  message.value = '';
+  // Reset Submit button
+  submitBtn.textContent = "Submit";
+  submitBtn.removeAttribute('disabled');
+});
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('Send Clicked');
+
+  // Change button style
+  submitBtn.textContent = "Sent";
+  submitBtn.setAttribute('disabled', 'disabled');
+  
+  // Handle Form Submission
+  
+
+});
